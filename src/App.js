c5c257f2c5099117d854156f9fb6592169ab5689
TCA-UW <uwtechconsulting@gmail.com>
2025-03-09 21:04:53 -0700

    ghp fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import FAQ from './components/FAQ';
 import Members from './components/Members'

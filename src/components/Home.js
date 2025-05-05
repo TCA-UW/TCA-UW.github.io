42718d4fb7892d5edd7341110737ab5c2eb73f2f
akshaisrin <akshaisrin@gmail.com>
2025-05-04 23:12:55 -0700

    Created optimized build

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ const Home = () => {
     <div className="home-container">
       <header className="Text-block">
         {/* <Banner /> */}
+        
         <h1>Technology Consulting Association</h1>
         <p>At the University of Washington</p>
       </header>
